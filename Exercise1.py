e4532de634e2fc54a47ad84af20a9ecc9f0c6a52
kiruhs <87802917+kiruhs@users.noreply.github.com>
2025-02-14 07:57:33 +0200

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,91 @@
+x = 3
+y = x
+
+
+# Types of data
+# The following types are not mutual
+# Integer - int - 3; -34
+# Float   - float - 0.12; 100.3, 10.0
+# Boolean - bool  - True, False
+# tuple(кортеж) - tuple - (1, 3, 6)
+# None (ничто) - NoneType
+# string - str - "Hello, world!"
+
+# The following types are mutual
+# list(список) - list - [1, 2, 2, 2]
+# set(множество/набор) - set - {2, 3}
+# dictionary(словарь) - dict - {2:4, 6:89}
+# user-defined classes
+
+# name = 'Alexander'
+# num = 20
+# num2 = num
+# num3 = 20
+# print(id(num))
+# print(id(num2))
+# print(id(num3))
+# print(id(name))
+# print(id('Alexander'))
+
+# long_str = """This is a
+#  very,
+#  very,
+#  long
+#     string"""
+# print(long_str)
+# print(type(long_str))
+
+# print(name)
+# print(name.upper())
+# print(name.lower())
+# print(name.capitalize())
+# print(name.count('a'))
+# print(dir(name))
+
+# help(name.count)
+# print(len(name))
+# print(name[0:4])
+# print(name[:4])
+# print(name[4:])
+# print(name[1:6:2])
+#
+# print(name[::2])
+#
+# print(name[::-1])
+# print(name[-6])
+# print(name[5::-1])
+
+# num = int(input("Enter the number: "))
+# num = input("Enter the number: ")
+# print(num)
+# # print(type(num))
+# print(int(num) + 100)
+import math
+# print(int(pow(4.0,3.0)))
+# print(pow(4,0.5))
+# x = 4
+# print(pow(x, 2))
+# print = "hello"  - Don't do it!!!
+# popul = 9_000_000_001.5
+# half = popul / 2
+# print(half)
+# print(round(half))
+# print(math.ceil(half))
+# print(math.floor(half))
+
+# print(dir(math))
+
+# print(pow(2,-2))
+# print(math.pi)
+
+# exist = True
+# print(exist)
+# print(type(exist))
+# print(dir(exist))
+
+# print(5 <= 3)
+
+# print("Hello" == "Hello ")
+
+# print("Hello" == 'Hello')
+print("Hella" > "HellZ")
