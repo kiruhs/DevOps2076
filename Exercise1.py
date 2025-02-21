58db5f2246fc0d18a6d2e4474f628439e7558e99
kiruhs <87802917+kiruhs@users.noreply.github.com>
2025-02-21 11:29:54 +0200

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -303,3 +303,179 @@ while i < len(lst1) and len(lst2):
 print(lst1)
 print(lst2)
 print(lst3)
+
+# 20.02.2025
+
+
+# lst = [56, 13, 37, 0, 5, 37, 34, 0, -4]
+#
+# for j in range(len(lst)-1):
+#     for i in range(len(lst)-1):
+#         if lst[i] > lst[i+1]:
+#             lst[i], lst[i+1] = lst[i+1], lst[i]
+#
+# print(lst)
+
+# num = int(input("Enter number of lines: "))
+# pascal = []
+# for i in range(num):
+#     pascal.append([1]+[0]*num)
+#
+# for i in range(1,num):
+#     for j in range(1, i+1):
+#         pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1]
+#
+# for i in range(num):
+#     print(" " * (num - i -1), end="")
+#     for j in range(i+1):
+#         print(pascal[i][j], end=" ")
+#     print()
+
+# print(pascal)
+
+# tuple - Кортеж
+
+# tpl = ("apple", "banana", "cherry")
+# print(id(tpl))
+# tpl2 = ("cherry", "apple", "banana")
+# print(tpl == tpl2)
+# print(type(tpl))
+# print(tpl[1])
+# print(dir(tpl2))
+# print(tpl.index("cherry"))
+
+# for i in tpl:
+#     print(i)
+
+# RAM - random access memory
+
+# tpl += tpl2
+# print(tpl)
+# print(id(tpl))
+
+# first_hundred = []
+# print(type(first_hundred))
+# for i in range(100):
+#     first_hundred.append(i)
+#
+# print(first_hundred)
+# first_hundred = tuple(first_hundred)
+# print(first_hundred)
+
+
+import time
+# lst = [ i for i in range(50_000_000)]
+# lst = list(range(50_000_000))
+# tpl = tuple(range(50_000_000))
+# print(lst)
+# print(tpl)
+# print(lst.__sizeof__())
+# gen = ( i for i in range(5))
+# print(gen.__sizeof__())
+# tpl = tuple(gen)
+# print(tpl.__sizeof__())
+
+# start = time.time()
+# for i in range(len(tpl)):
+#     for j in range(10):
+#         a = tpl[i]
+# print("Total lookup time for tuple: ", time.time() - start)
+#
+# start = time.time()
+# for i in range(len(lst)):
+#     for j in range(10):
+#         a = lst[i]
+# print("Total lookup time for list: ", time.time() - start)
+
+# green = "apple"
+# fruits = (green, "banana", "cherry", "kiwi", "plum")
+# print(fruits)
+#
+# *green, yellow, red, blue = fruits
+# print(green)
+
+# tpl = ((1,2),(3,4),(5,6))
+# print(tpl[1][1])
+#
+# tpl3 = ([1,2],(3,4))
+# print(tpl3)
+# print(tpl3[0])
+# print()
+#
+# tpl3[0][1] = ["Hello, world", (4,9), [[("Hello", "World"), True], None, -34.56]]
+# tpl3[0][1][2][0][0] = 5
+# print(tpl3)
+
+# inp = [(6,7), (4,5), (2,3), (2,8)]
+# # output [(2,3), (4,5), (2,8), (6,7)]
+#
+# lst = len(inp)
+# for i in range(lst):
+#     for j in range(lst-i-1):
+#         if inp[j][0] + inp[j][1] > inp[j+1][0] + inp[j+1][1]:
+#             inp[j], inp[j+1] = inp[j+1], inp[j]
+#
+# print(inp)
+
+# x = [5,6,7]
+#
+# print(not isinstance(x, (int, str, float)))
+
+# lst = [4, 5, 7, "Hello, Buddy", [], (1,2,3), 5, 56, True]
+# # print(lst[3])
+#
+# cnt = 0
+# for num in lst:
+#     if isinstance(num, tuple):
+#         break
+#     cnt += 1
+#
+# print(cnt)
+
+# time.time()
+
+
+# def my_first():
+#     print("Hello")
+#
+# x = my_first()
+# p = my_first
+# print(x)
+
+# def hello(name, city):
+#     print("Hello", name)
+#     print("You live in", city)
+#
+# hello("Alexander", "Ashdod")
+
+
+
+# def max2(x, y):
+#     if x > y:
+#         return x
+#     else:
+#         return y
+#
+# # print(max2(23, 67))
+#
+# def max3(x, y, z):
+#     tmp = max2(y, z)
+#
+#     return max2(x, tmp)
+#
+# print(max3(67, 100, 0))
+
+
+# def fun_country(country="You forgot to put country"):
+#     print("I am from " + country)
+#
+# fun_country("Norway")
+# fun_country("USA")
+# fun_country()
+
+def call_child(*kids):
+    print("The name of child is ")
+    for i in kids:
+        print(i)
+
+call_child("Mary", "Sam", "Bob", "Emily")
